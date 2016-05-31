ec037e1bac0d6c6f83643c57cc942b125f7ac32b
Anderson Lima <andersoncardosolima0@gmail.com>
2016-05-31 08:52:45 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/aluno/trabalhofinal/CadProdutosActivity.java
@@ -0,0 +1,13 @@
+package com.example.aluno.trabalhofinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadProdutosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cad_produtos);
+    }
+}
---
+++ app/src/main/java/com/example/aluno/trabalhofinal/InfPessoaisActivity.java
@@ -0,0 +1,13 @@
+package com.example.aluno.trabalhofinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InfPessoaisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inf_pessoais);
+    }
+}
